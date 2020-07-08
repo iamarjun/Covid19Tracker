82899d2a1d3594ee09409ba658aeb3959c17a662
iamarjun <arjunmanoj1995@gmail.com>
2020-07-08 11:34:25 +0530

    add helper function for viewbinding

---
+++ app/src/main/java/com/arjun/covid19tracker/util/viewbinding/viewBinding.kt
@@ -0,0 +1,12 @@
+package com.arjun.covid19tracker.util.viewbinding
+
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T
+) =
+    lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }
