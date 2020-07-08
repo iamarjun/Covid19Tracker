4bd66c158cd082677ff791336b51d062961e9284
iamarjun <arjunmanoj1995@gmail.com>
2020-07-08 12:09:43 +0530

    add logging (Tiber) and some handy extension func

---
+++ app/src/main/java/com/arjun/covid19tracker/util/extensions.kt
@@ -0,0 +1,20 @@
+package com.arjun.covid19tracker.util
+
+import android.app.Activity
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun toVisibility(constraint: Boolean): Int = if (constraint) {
+    View.VISIBLE
+} else {
+    View.GONE
+}
+
+fun Fragment.showToast(text: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), text, length).show()
+}
+
+fun Activity.showToast(text: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, length).show()
+}
